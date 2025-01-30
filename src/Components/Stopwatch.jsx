a67e04362a491ac90c1705332ba245d1cad0c698
Joseph <jokyriakides@msn.com>
2025-01-30 15:45:08 +0000

    Implemented start, restart function in stopwatch

--- src/Components/Stopwatch.jsx
+++ src/Components/Stopwatch.jsx
@@ -1,13 +1,34 @@
-import React, {useState} from "react"
+import React, { useEffect, useRef, useState } from "react"
 
+
+//MVP 1 - Restart button
 export default function Stopwatch() {
-  return (
-    <>
-      <h1> 0 </h1>
-      <button className="restart"> Restart </button>
-      <button className="start"> Start </button>
-      <button className="pause"> Pause </button>
-      <button className="lap"> Lap </button>
-    </>
-  )
-}
+  const [time, setTime] = useState(0)
+  function handleRestart() {
+    setTime(0)
+  }
+  useEffect(() => {
+    timeInc()
+
+    return () => clearInterval(timeHandler.current)
+
+    })
+
+    let timeHandler=useRef()
+
+    function timeInc(){
+      timeHandler.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1)
+          } , 1000)
+        }
+
+    return (
+      <>
+        <h1> {time} </h1>
+        <button className="restart" onClick={()=>setTime(0)}> Restart </button>
+        <button className="start"> Start </button>
+        <button className="pause"> Pause </button>
+        <button className="lap"> Lap </button>
+      </>
+    )
+  }
