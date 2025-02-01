a76c43675e9e02068acc777d3161d906e4d1e9c2
Jackafella1 <jackafella@gmail.com>
2025-02-01 11:44:52 +0000

    Fix build and update paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import {useState} from "react"
-import React from "react"
+//import {useState} from "react"
+//import React from "react"
 
 import "./App.css"
 import Stopwatch from "./Components/Stopwatch.jsx"
